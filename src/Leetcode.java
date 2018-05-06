16c6f50cc159b90e75213fa758c1eb34fe3948d4
huizhaowang <913967754@qq.com>
2018-05-06 18:54:00 +0800

    2018-05-06 晚 实验室 + twoSum

---
+++ src/Leetcode.java
@@ -0,0 +1,30 @@
+public class Leetcode {
+
+
+    public int[] twoSum(int[] nums, int target) {
+        int[] index_two = new int[2];
+        out:
+        for(int i =0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if ((nums[i]+nums[j])==target){
+                    index_two[0] = i;
+                    index_two[1] = j;
+                    break out;
+                }
+            }
+        }
+        return index_two;
+    }
+
+
+    public static void main(String args[]){
+
+        Leetcode myLeetcode = new Leetcode();
+
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] index_two = myLeetcode.twoSum(nums,target);
+        System.out.println("["+index_two[0]+","+index_two[1]+"]");
+    }
+
+}
