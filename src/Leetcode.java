885ef86f0aa8d177cd7a960c1c076ccee6c663ad
huizhaowang <913967754@qq.com>
2018-05-07 12:01:09 +0800

    运用Hashmap加快代码运行速度

--- src/Leetcode.java
+++ src/Leetcode.java
@@ -1,7 +1,10 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Leetcode {
 
 
-    public int[] twoSum(int[] nums, int target) {
+    public int[] my_twoSum(int[] nums, int target) {
         int[] index_two = new int[2];
         out:
         for(int i =0;i<nums.length;i++){
@@ -16,6 +19,21 @@ public class Leetcode {
         return index_two;
     }
 
+    public int[] twoSum(int[] nums,int target){
+
+        Map<Integer,Integer> numsMap = new HashMap<Integer, Integer>();
+        for (int i = 0; i <nums.length ; i++) {
+            numsMap.put(nums[i],i);
+        }
+        for (int i = 0; i <nums.length ; i++) {
+            int complement = target - nums[i];
+            if(numsMap.containsKey(complement)&&numsMap.get(complement)!=i){
+                return new int[]{i, numsMap.get(complement)};
+            }
+        }
+        System.out.println("数组里面没这两个数！");
+        return null;
+    }
 
     public static void main(String args[]){
 
@@ -24,7 +42,7 @@ public class Leetcode {
         int[] nums = {2, 7, 11, 15};
         int target = 9;
         int[] index_two = myLeetcode.twoSum(nums,target);
-        System.out.println("这是我的leetcode!");
+        System.out.println("["+index_two[0]+","+index_two[1]+"]");
         
     }
 
