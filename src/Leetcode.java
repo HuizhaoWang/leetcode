76f01c4b9caa279861a1e92611c455d944a362ae
huizhaowang <913967754@qq.com>
2018-05-08 16:19:06 +0800

    add two sum

--- src/Leetcode.java
+++ src/Leetcode.java
@@ -1,11 +1,15 @@
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
+import javax.print.attribute.standard.NumberUp;
 import java.util.HashMap;
 import java.util.Map;
 
 class ListNode{
-    public int val;
-    public ListNode next;
-    public ListNode(int val) {
+    int val;
+    ListNode next;
+    ListNode(int val) {
         this.val = val;
+        this.next = null;
     }
 }
 
@@ -28,7 +32,7 @@ public class Leetcode {
 
     public int[] twoSum(int[] nums,int target){
 
-        Map<Integer,Integer> numsMap = new HashMap<Integer, Integer>();
+        Map<Integer,Integer> numsMap = new HashMap<>();
         for (int i = 0; i <nums.length ; i++) {
             numsMap.put(nums[i],i);
         }
@@ -42,7 +46,7 @@ public class Leetcode {
         return null;
     }
 
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    public ListNode my_addTwoNumbers(ListNode l1, ListNode l2) {
         //先排除两个数有0值的情况
         if (l1.val==0){
             return l2;
@@ -51,63 +55,83 @@ public class Leetcode {
             return l1;
         }
 
+        ListNode listNode = l1;
         int flag = 0;
         while (l1!=null&&l2!=null){
             if (l1.val+l2.val+flag>=10){
                 l1.val = l1.val+l2.val+flag-10;
+                flag =1;
             }else {
                 l1.val = l1.val+l2.val+flag;
+                flag = 0;
             }
             l1=l1.next;
             l2=l2.next;
         }
-
-        if (l1==null){
-            while (flag==1&&l2.val==9&&l2!=null){
-                ListNode listNode = new ListNode(0);
-                l1 = listNode;
-                l2 = l2.next;
-            }
-            ListNode listNode = new ListNode(0);
-            l1 = listNode;
-            l1.val=1+l2.val;
-
-            l1.next = l2;
-        }
-
-        if (l2==null){
-            while(flag==1&&l1.val==9&&l1!=null){
-                l1.val = 0;
-                l1 = l1.next;
-            }
-            l1.val +=flag;
-        }
-        return l1;
+        return listNode;
     }
 
-    public String printAll(ListNode listNode){
+    public ListNode addTwoNumbers(ListNode l1,ListNode l2){
+        ListNode dummyHead = new ListNode(0);
+        ListNode p = l1, q = l2, curr = dummyHead;
+        int carry = 0;
+        while (p != null || q != null) {
+            int x = (p != null) ? p.val : 0;
+            int y = (q != null) ? q.val : 0;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null) p = p.next;
+            if (q != null) q = q.next;
+        }
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return dummyHead.next;
+    }
+
+    public void printAll(ListNode listNode){
         String num = "";
         while(listNode!=null){
-            num += listNode.val;
+            num = num + listNode.val;
             listNode = listNode.next;
         }
-        return num;
+        System.out.println(num);
     }
 
+    public ListNode ArrayToListNode(int[] num){
+        ListNode listNode = new ListNode(0);
+        ListNode head = listNode;
+        for (int i=0;i<num.length;i++){
+            listNode.next = new ListNode(num[i]);
+            listNode = listNode.next;
+        }
+        return head;
+    }
     public static void main(String args[]){
 
         Leetcode myLeetcode = new Leetcode();
 
-
-
-        /*twoSum
+        //twoSum
+        /*
         int[] nums = {2, 7, 11, 15};
         int target = 9;
         int[] index_two = myLeetcode.twoSum(nums,target);
         System.out.println("["+index_two[0]+","+index_two[1]+"]!");
         */
 
+        //addTwoNumbers
 
+        int[] num1 = {2,4,3,5};
+        int[] num2 = {5,6,4};
+
+        ListNode listNode1 = myLeetcode.ArrayToListNode(num1);
+        ListNode listNode2 = myLeetcode.ArrayToListNode(num2);
+
+        ListNode myListNode = myLeetcode.addTwoNumbers(listNode1.next,listNode2.next);
+
+        myLeetcode.printAll(myListNode);
 
     }
 
